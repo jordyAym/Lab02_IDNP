0170f33976cbce8baa6c991307741319f05fe68f
jordyAym <jayma@unsa.edu.pe>
2024-09-25 13:34:24 -0500

    Se crea variables para recuperar los valores del formulario y un objeto que será enviado

--- LoginSample/app/src/main/java/com/example/loginsample/AccountActivity.java
+++ LoginSample/app/src/main/java/com/example/loginsample/AccountActivity.java
@@ -3,6 +3,7 @@ package com.example.loginsample;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -23,6 +24,31 @@ public class AccountActivity extends AppCompatActivity {
             return insets;
         });
 
+        Button btnAceptar = findViewById(R.id.btnAceptar);
+        Button btnCancelar = findViewById(R.id.btnCancelar);
 
+        //Creación de variables para recuperar los valores del formulario
+
+        EditText edtFirstName = findViewById(R.id.edtFirstname);
+        EditText edtLastname = findViewById(R.id.edtLastname);
+        EditText edtEmail = findViewById(R.id.edtEmail);
+        EditText edtPhone = findViewById(R.id.edtPhone);
+        EditText edtUsername2 = findViewById(R.id.edtUsername2);
+        EditText edtPassword2 = findViewById(R.id.edtPassword2);
+
+
+        btnAceptar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Aquí se recuperan los datos del formulario
+                AccountEntity accountEntity =  new AccountEntity();
+                accountEntity.setFirstname(edtFirstName.getText().toString());
+                accountEntity.setLastname(edtLastname.getText().toString());
+                accountEntity.setEmail(edtEmail.getText().toString());
+                accountEntity.setPhone(edtPhone.getText().toString());
+                accountEntity.setUsername(edtUsername2.getText().toString());
+                accountEntity.setPassword(edtPassword2.getText().toString());
+            } 
+        });
     }
 }
